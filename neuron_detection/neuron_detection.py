7d4221400c360fea73dc956fa9230f14914496a6
Kamil Burda <8431034+kamilburda@users.noreply.github.com>
2025-10-14 15:32:18 +0200

    neuron_detection: Fix syntax error

--- neuron_detection/neuron_detection.py
+++ neuron_detection/neuron_detection.py
@@ -49,10 +49,10 @@ def main(argv):
 
     lines = []
     file_path = "./corpus_all/"+argv[0] + ".txt"
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        lines = random.sample(lines, int(argv[1]))
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    lines = [line.strip() for line in lines]
+    lines = random.sample(lines, int(argv[1]))
 
 
     candidate_premature_layers = []
