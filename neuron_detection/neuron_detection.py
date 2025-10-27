2cd4e39203a8b5760e691366fb8a48a087e1fedf
Kamil Burda <8431034+kamilburda@users.noreply.github.com>
2025-10-27 12:25:18 +0100

    Minor refactoring

--- neuron_detection/neuron_detection.py
+++ neuron_detection/neuron_detection.py
@@ -23,17 +23,17 @@ def Prompting(model, tokenizer, prompt):
     layer_keys = None
 
 
-    def mlp_hook(model_, _args, output):
-        hidden_state = _args[0]
+    def mlp_hook(module, args, output):
+        hidden_state = args[0]
 
         return (
             output, 
-            torch.sum(torch.abs(model_.up_proj(hidden_state)), dim=1).squeeze().tolist(),
-            torch.sum(torch.abs(model_.up_proj(hidden_state)), dim=1).squeeze().tolist(),
+            torch.sum(torch.abs(module.up_proj(hidden_state)), dim=1).squeeze().tolist(),
+            torch.sum(torch.abs(module.up_proj(hidden_state)), dim=1).squeeze().tolist(),
         )
 
 
-    def store_activated_neurons_hook(model_, _args, output):
+    def store_activated_neurons_hook(module, _args, output):
         nonlocal hidden_states_as_lists, logits_dict, activate_keys_fwd_up, activate_keys_fwd_down, activate_keys_q, activate_keys_k, activate_keys_v, activate_keys_o, layer_keys
 
         logits_dict, outputs, activate_keys_fwd_up, activate_keys_fwd_down, activate_keys_q, activate_keys_k, activate_keys_v, activate_keys_o, layer_keys = output
